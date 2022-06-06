4983654a92aa232daf2ec5e9415463f34bc13347
kjh9571 <jhkim9571@gmail.com>
2022-06-06 22:59:28 +0900

    Practice problem(Adventurer's Guild)

---
+++ Algorithm/greedy/practice_3.py
@@ -0,0 +1,14 @@
+n = int(input())
+fears = list(map(int, input().split()))
+fears.sort()
+cnt = 0 # 총 그룹의 수
+grp = 0 # 현재 그룹에 포함된 모험가의 수
+
+for fear in fears: # 공포도를 낮은 것부터 하나씩 확인하며
+    cnt += 1 # 현재 그룹에 해당 모험가를 포함시키기
+    if cnt >= fear: # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+        grp += 1 # 총 그룹의 수 증가시키기
+        cnt = 0 # 현재 그룹에 포함된 모험가의 수 초기화
+
+print(grp)
+        
